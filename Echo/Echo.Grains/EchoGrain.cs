528ca85154e675bc17f8878ac742e8652be8fefe
Anibal Velarde <anibalvelarde@gmail.com>
2017-11-27 17:27:42 -0600

    Added some counters to keep track of stuff.

--- Echo/Echo.Grains/EchoGrain.cs
+++ Echo/Echo.Grains/EchoGrain.cs
@@ -6,18 +6,36 @@ using System.Threading.Tasks;
 
 namespace Echo.Grains
 {
-	public sealed class EchoGrain
-		: Grain, IEchoGrain
-	{
-		public async Task SpeakAsync(EchoSpeakMessage message)
-		{
-			if (message == null) { throw new ArgumentNullException(nameof(message)); }
+   public sealed class EchoGrain
+	   : Grain, IEchoGrain
+   {
+	  private int _callCounter = 0;
+	  private uint _msgRepeatCounter = 0;
 
-			for (var i = 0; i < message.Repeat; i++)
-			{
-				await Console.Out.WriteLineAsync(
-					$"{i} - {message.Message}");
-			}
-		}
-	}
+	  public async Task SpeakAsync(EchoSpeakMessage message)
+	  {
+		 if (message == null) { throw new ArgumentNullException(nameof(message)); }
+
+		 if (CanShowCounter()) { await Console.Out.WriteLineAsync($"Grain has spoken {_callCounter} times and has repeated {_msgRepeatCounter} messages."); }
+
+		 for (var i = 0; i < message.Repeat; i++)
+		 {
+			await Console.Out.WriteLineAsync(
+				$"{i} - {message.Message}");
+		 }
+
+		 UpdateCallCount(message.Repeat);
+	  }
+
+	  private void UpdateCallCount(uint repeatCount = 0)
+	  {
+		 _callCounter++;
+		 _msgRepeatCounter += repeatCount;
+	  }
+
+	  private bool CanShowCounter()
+	  {
+		 return ((_callCounter != 0) && ((_callCounter % 2) == 0));
+	  }
+   }
 }
